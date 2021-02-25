fa1d5d5910371f3892b388421416ced477f16294
GTLiSunnyi <62019863+GTLiSunnyi@users.noreply.github.com>
2021-02-25 12:07:03 +0800

    Update logger.go

--- random-seed/common/logger.go
+++ random-seed/common/logger.go
@@ -16,22 +16,3 @@ func init() {
 
 	Logger.SetLevel(log.InfoLevel)
 }
-=======
-package common
-
-import (
-	"os"
-
-	log "github.com/sirupsen/logrus"
-)
-
-// Logger is a logger instance
-var Logger = log.New()
-
-func init() {
-	Logger.SetFormatter(&log.TextFormatter{FullTimestamp: true})
-
-	Logger.SetOutput(os.Stdout)
-
-	Logger.SetLevel(log.InfoLevel)
-}
